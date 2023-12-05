289e4ef725a27c7c444fda616f7ccab66e45cf66
enesdogan00 <enesdogan26@gmail.com>
2023-12-05 21:20:07 +0300

    added delete

--- app/twitter/router.py
+++ app/twitter/router.py
@@ -1,3 +1,4 @@
+from beanie import PydanticObjectId
 from fastapi import APIRouter
 
 from app.general.constants import Success
@@ -6,12 +7,31 @@ from app.twitter.model import Twitter, TwitterPost
 
 router = APIRouter()
 
+@router.get("/account")
+async def get_twitter_account() -> list[Twitter]:
+    return await Twitter.find().to_list()
+
+
 @router.post("/account")
 async def create_twitter_account(data: Twitter) -> BaseResponse:
     await data.save()
     return BaseResponse(data=data, message=Success.created)
 
+@router.delete("/account/{id}")
+async def delete_twitter_account(id: PydanticObjectId) -> BaseResponse:
+    await Twitter.find_one(Twitter.id == id).delete()
+    return BaseResponse(message=Success.deleted)
+
+@router.get("/post")
+async def get_twitter_post() -> list[TwitterPost]:
+    return await TwitterPost.find().to_list()
+
 @router.post("/post")
 async def create_twitter_post(data: TwitterPost) -> BaseResponse:
     await data.save()
     return BaseResponse(data=data, message=Success.created)
+
+@router.delete("/post/{id}")
+async def delete_twitter_post(id: PydanticObjectId) -> BaseResponse:
+    await TwitterPost.find_one(TwitterPost.id == id).delete()
+    return BaseResponse(message=Success.deleted)
