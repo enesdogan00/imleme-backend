5ddb34fbdd67c891b671140c2d325106b3d8d60e
enesdogan00 <enesdogan26@gmail.com>
2024-01-23 22:47:57 +0300

    added max of 10 to random post

--- app/mixins/posts.py
+++ app/mixins/posts.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from random import randint
 from beanie import Link
 from pydantic import BaseModel
@@ -8,12 +9,34 @@ class PostMixin(BaseModel):
     rss_id: Link[RSS] = None
     @classmethod
     async def random(cls):
-        count = await cls.find(cls.sent == False).count()
-        random_index = randint(0, count - 1)
-        random_document = (
-            await cls.find(cls.sent == False).skip(random_index).limit(1).to_list(1)
-        )[0]
-        await random_document.fetch_link(cls.rss_id)
-        feed = random_document.rss_id
-        print(feed.daily_count)
+        pipeline = [
+  {
+    "$match": {
+      "sentDate": {
+        "$gte": datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+      }
+    }
+  },
+  {
+    "$group": {
+      "_id": "$rss_id",
+      "count": { "$sum": 1 }
+    }
+  },
+  {
+    "$sort": {
+      "count": -1
+    }
+  },
+  {
+    "$limit": 10
+  },
+   {"$sample": {"size": 1}}
+]
+        random_rss = (
+            await cls.aggregate(pipeline).to_list()
+        )
+        random_rss = random_rss[0]["_id"]
+        random_document = await cls.aggregate([{"$match": {"rss_id": random_rss}}, {"$sample": {"size": 1}}]).to_list(1)
+        random_document = random_document[0]
         return random_document
